e3e516b9397193d67d99d5ca64535d5b97cfabfd
pkienscherf <philipp.kienscherf@gmail.com>
2021-04-16 19:38:01 +0200

    Update requirements management

--- setup.py
+++ setup.py
@@ -7,6 +7,11 @@ setup(
     author="Student",
     author_email="student@uni-koeln.de",
     packages=["yellowcab"],
+    install_requires=[
+        'pandas',
+        'click',
+        'scikit-learn'
+    ],
     entry_points={
         'console_scripts': ['yellowcab=yellowcab.cli:main']
     }
