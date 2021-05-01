4623e449235d6a3b3dd856d94c44b3df9b13a874
Diem Ly <kyralee330@gmail.com>
2021-05-01 23:22:59 +0300

    Update geofile.py

--- data/input/geofile.py
+++ data/input/geofile.py
@@ -1,10 +1,19 @@
 import geojson
+from shapely.geometry import shape
 
 def map():
     with open('input/taxi_zones.geojson') as f:
         gj = geojson.load(f)
-    features = gj['features']
-    return features
+
+    for i in gj['features']:
+        if 'Queens' in i['properties'].values():
+            return i['geometry']
+
+    # c = [i['geometry']['coordinates'] for i in gj['features'] if "Queens" in i['properties'].values()]
+
+def main():
+    x = map()
+    print(shape(x).centroid)
 
 if __name__ == '__main__':
     main()
