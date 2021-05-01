683a8aca3b9e5b4316985f23e068bd41e8084ea9
Diem Ly <kyralee330@gmail.com>
2021-05-01 23:26:28 +0300

    Update geofile.py

--- data/input/geofile.py
+++ data/input/geofile.py
@@ -2,9 +2,11 @@ import geojson
 from shapely.geometry import shape
 
 def map():
+    # Open geojson file
     with open('input/taxi_zones.geojson') as f:
         gj = geojson.load(f)
 
+    # Extract geo-information of Queens borough
     for i in gj['features']:
         if 'Queens' in i['properties'].values():
             return i['geometry']
@@ -13,6 +15,7 @@ def map():
 
 def main():
     x = map()
+    # Return central point
     print(shape(x).centroid)
 
 if __name__ == '__main__':
