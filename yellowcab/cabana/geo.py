a47dbb86dbcf0f36c6ac875440e98708b7b3300b
Diem Ly <kyralee330@gmail.com>
2021-05-15 16:39:39 +0300

    Create visualization function in geo module

--- yellowcab/cabana/geo.py
+++ yellowcab/cabana/geo.py
@@ -2,24 +2,28 @@ import geojson
 import numpy as np
 from shapely.geometry import shape
 from shapely.geometry import Point
+import shapely.ops as so
+from descartes import PolygonPatch
+import matplotlib.pyplot as plt
 
-# Module returns a dictionary of location ID as keys and central points as values
+
+# Module using geo data to return information
+    # get_centroid() returns a dictionary of location ID as keys and central points as values
+    # get_area(location) illustrates area and its central point
+    # get_map() prints NYC's map
+#-------------------------------------------------------------------------------
 
 class geo:
     # Get geo data of a specific location
     def __init__(self):
-        pass
 
         with open(r'C:\Users\kyral\Documents\GitHub\PDS_Yellowcab_UoC\data\input\taxi_zones.geojson') as f:
             self.d = geojson.load(f)
 
-        # self.data = [i for i in self.d['features'] if 'Queens' in i['properties'].values()]
         self.l = np.array([i['properties']['LocationID'] for i in self.d['features']])
 
     # Find central point of a specific location
     def get_centroid(self):
-        # index = np.where(self.l == self.location)
-        # i = index[0].item()
         center = []
         for i in range(len(self.l)):
             t = shape(self.d['features'][i]['geometry']).centroid
@@ -27,6 +31,32 @@ class geo:
             center.append([point.x,point.y])
         return dict(zip(self.l,center))
         # return len(self.l)
+
+    # Map one areas based on location ID
+    def get_area(self,location = 22):
+        index = np.where(self.l == location)
+        i = index[0].item()
+        poly = self.d['features'][i]['geometry']
+        point = self.get_centroid()[location]
+
+        BLUE = '#6699cc'
+        fig = plt.figure()
+        ax = fig.gca()
+        ax.plot(point[0],point[1], 'ro')
+        ax.add_patch(PolygonPatch(poly, fc=BLUE, ec=BLUE, alpha=0.5, zorder=2))
+        ax.axis('scaled')
+        plt.show()
+
+    # Map NYC areas
+    def get_map(self):
+        poly = [self.d['features'][i]['geometry'] for i in range(len(self.l))]
+        BLUE = '#6699cc'
+        fig = plt.figure()
+        ax = fig.gca()
+        for p in poly:
+            ax.add_patch(PolygonPatch(p, fc=BLUE, ec=BLUE, alpha=0.5, zorder=2))
+        ax.axis('scaled')
+        plt.show()
 
 
 
--- yellowcab/test.py
+++ yellowcab/test.py
@@ -4,32 +4,34 @@ import datetime
 
 
 def main():
-    # Test geo() module, giving a dict of approx. 260 keys
-    # data = geo()
+        # Test geo() module, giving a dict of approx. 260 keys
+    data = geo()
     # print(data.get_centroid())
+    print(data.get_area())
+    # print(data.get_map())
 #-------------------------------------------------------------
-    # Test trip_inputs() module
-    x = trips()
+        # Test trip_inputs() module
+    # x = trips()
     # l = x.get_borough_locationID(borough="Bronx")
     # print(l)
 
-    t = x.get_trips(fraction=0.05)
+    # t = x.get_trips(fraction=0.05)
     # print(t.info())
     # print(t.head())
 #-------------------------------------------------------------
-    # Test trips_info() module
-    y = trips_info(t)
+        # Test trips_info() module
+    # y = trips_info(t)
     # df = y.get_time(column='tpep_dropoff_datetime')
     # print(df.info())
     # print(df.head())
-    #
+
     # d = y.get_duration()
     # print(d.head())
     # print(d.info())
-    #
-    df = y.get_position()
-    print(df.info())
-    print(df.head())
+
+    # df = y.get_position()
+    # print(df.info())
+    # print(df.head())
 
     # m = y.get_aggregate('month')
     # h = y.get_aggregate('hour')
